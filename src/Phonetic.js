8fa85d07cc150b29a8cc6716af997dc9e7283e0c
Ciara R. <100184461+creed725@users.noreply.github.com>
2026-01-13 20:45:00 -0600

    Styled the application

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,6 +1,11 @@
 import React from "react";
+import "./Phonetic.css";
 
 export default function Phonetic(props) {
   console.log(props.phonetic);
-  return <div className="Phonetic">{props.phonetic}</div>;
+  return (
+    <div className="Phonetic">
+      <span className="text">{props.phonetic}</span>
+    </div>
+  );
 }
