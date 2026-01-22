055cdc233a1d982c7af717d8794606f04454bb73
Ciara R. <100184461+creed725@users.noreply.github.com>
2026-01-21 20:49:54 -0600

    Rendered the photos of the word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionResponse(response) {
     setResults(response.data);
+  }
+
+  function handlePexelsResponse(response) {
+    //console.log(response.data);
+    setPhotos(response.data.photos);
   }
 
   function search() {
     // documentation: https://www.shecodes.io/learn/apis/dictionary
     let apiKey = `40bdb8c3a26579atfoa8a2d376def906`;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionResponse);
+
+    let pexelsApiKey =
+      "GbcuvCod3hvE4ZFVR3K2gUKHNHMPPGr6mmSMn7ffmVJZNybc2BIWcJkW";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: pexelsApiKey };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -39,14 +52,18 @@ export default function Dictionary(props) {
         <section>
           <h1>What word do you want to look up?</h1>
           <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} 
-            defaultValue={props.defaultKeyword}/>
+            <input
+              type="search"
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
           </form>
           <div className="hint">
             suggested words: sunset, wine, yoga, forest...
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  console.log(props.phonetic);
+  //console.log(props.phonetic);
   return (
     <div className="Phonetic">
       <span className="text">{props.phonetic}</span>
